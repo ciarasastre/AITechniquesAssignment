cc64185e5ecd665df058cf3a3403b0ebd1bb5a59
ciarasastre <sastreciara@hotmail.com>
2019-05-05 20:54:25 +0100

    Added bee swarm mechanism

---
+++ HoneyParkBackup/Assets/Scripts/Flock.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public float speed = 0.001f;
+    float rotationSpeed = 4.0f;
+    Vector3 averageHeading; // Where they are going / heading towards
+    Vector3 averagePosition;
+    float neighbourDistance = 3.0f;
+
+    bool turning = false; // makes them stay within boundries
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Makes the flock move differently EG some move faster than others
+        speed = Random.Range(0.5f, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Testing distance of bees pos and hives center
+        if(Vector3.Distance(transform.position, Vector3.zero) >= GlobalFlock.hiveSize)
+        {
+            turning = true;
+        }
+        else
+        {
+            turning = false;
+        }
+
+        //Turn around and go back
+        if (turning)
+        {
+            Vector3 direction = Vector3.zero - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                    Quaternion.LookRotation(direction),
+                                                    rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            // Rules get called at certain times
+            if (Random.Range(0, 5) < 1)
+            {
+                ApplyRules();
+            }
+        }
+        //Makes the bees go forward
+        transform.Translate(0,0,Time.deltaTime * speed);
+    }
+
+    void ApplyRules()
+    {
+        //Find out all the bees
+        GameObject[] gos; //gos = gameobjects
+        gos = GlobalFlock.allBees;
+
+        //Then calculate center of a group and avoidance
+        Vector3 vcentre = Vector3.zero; // Center of group
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.1f;
+
+        Vector3 goalPos = GlobalFlock.goalPos;
+
+        float dist;
+
+        int groupSize = 0;
+
+        // Calculate group size
+        foreach(GameObject go in gos) //For each of the gameobjects
+        {
+            if(go != this.gameObject)
+            {
+                dist = Vector3.Distance(go.transform.position, this.transform.position);
+
+                //Get group size if your close to us your part of our group
+                if(dist <= neighbourDistance)
+                {
+                    vcentre += go.transform.position;
+                    groupSize++;
+
+                    //If we get too close we need to avoid each other
+                    if(dist < 1.0f)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    Flock anotherFlock = go.GetComponent<Flock>(); //Grab flock script from neighbours
+                    gSpeed = gSpeed + anotherFlock.speed; // Getting speed to get total speed
+                }
+
+            }
+        }
+
+        //If we are in a group
+        if(groupSize > 0)
+        {
+            //Calc average center and speed of group
+            vcentre = vcentre / groupSize + (goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            //Get direction
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+
+            if(direction != Vector3.zero)
+            {
+                //Change direction means change of rotation
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                        Quaternion.LookRotation(direction),
+                                                        rotationSpeed * Time.deltaTime);
+            }
+        }
+
+    }
+}
---
+++ HoneyParkBackup/Assets/Scripts/GlobalFlock.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFlock : MonoBehaviour
+{
+    public GameObject beePrefab;
+    public static int hiveSize = 10;
+    public static int numBees = 10;
+    public static GameObject[] allBees = new GameObject[numBees];
+
+    // This is if you want them to go to a specific location
+    //public GameObject goalPrefab;
+
+    public static Vector3 goalPos = Vector3.zero; // Goal position
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numBees; i++)
+        {
+            //Create bees and stick it in array around origin 
+            Vector3 pos = new Vector3(Random.Range(1, hiveSize),
+                                      Random.Range(1, hiveSize),
+                                      Random.Range(1, hiveSize));
+
+            allBees[i] = (GameObject)Instantiate(beePrefab, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Every couple of seconds the goal position is reset
+        if(Random.Range (0,10000) < 50)
+        {
+            goalPos = new Vector3(Random.Range(1, hiveSize),
+                                  Random.Range(1, hiveSize),
+                                  Random.Range(1, hiveSize));
+
+
+            // New Goal Prefab code here:
+            //Just add a sphere game object and your good to go!
+            //goalPrefab.transform.position = goalPos;
+        }
+    }
+}
